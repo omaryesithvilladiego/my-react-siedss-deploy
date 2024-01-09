24ec13089fedbf14f7ad5971d4fa3bbe7115c8a1
omaryesithvilladiego <123215875+omaryesithvilladiego@users.noreply.github.com>
2024-01-08 20:29:22 -0500

    last commit

--- src/pages/landing-page/LandingSiedss.jsx
+++ src/pages/landing-page/LandingSiedss.jsx
@@ -5,254 +5,254 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Unstable_Grid2';
 import style from "./headerlanding.module.css"
-
+import DvrIcon from '@mui/icons-material/Dvr';
+import { Button } from "@material-ui/core";
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 function Landing() {
   return (
-
-
     <>
-
       <Box>
         <HeaderLanding />
       </Box>
 
-      <Box style={{
-
-
-
-        width: '80%',
-        height: '100vh',
-        margin: '0 auto',
-        marginTop: '4rem'
-
-      }}>
-
+      <Box
+        style={{
+          width: "80%",
+          height: "100vh",
+          margin: "0 auto",
+          marginTop: "4rem",
+        }}
+      >
         <Grid container spacing={3}>
-
-          <Grid xs={12} sm={6} md={6}>
-
-            <Paper className={style.publicidadImg} sx={{
-              borderRadius: '1rem'
-            }} elevation={2}>
-
-
-              <Grid container spacing={5}>
+          <Grid  xs={12} sm={12} md={6} xl={3}>
+            <Paper
+              style={{ backgroundColor: "#1CFEBA" }}
+              className={style.publicidadImg}
+              sx={{}}
+              elevation={2}
+            >
+              <Grid
+                style={{ width: "90%", margin: "0 auto" }}
+                container
+                spacing={5}
+              >
                 <Grid xs={10} sm={6} item>
+                  <Paper
+                    
+                    style={{
+                      width:'fit-content',
+                      borderRadius: "1.5rem",
+                      backgroundColor:'transparent',
+                      padding:'1rem',
+                      display:'flex',
+                      flexDirection:'column',
+                      justifyContent:'center',
+                      alignItems:'center',
+                      backgroundColor:'white'
+                    }}
+                    elevation={1}
+                  >
 
-                  <Paper className={style.capturaFirst} style={{ width: '100%', height: '15rem', borderRadius: '1.5rem' }} elevation={1}>
 
+                    <DvrIcon style={{fontSize:'4rem'}} />
                   </Paper>
-
-
                 </Grid>
 
                 <Grid xs={12} md={12} sm={6} item>
+                  <Paper
+                    style={{ width: "100%", borderRadius: "1.5rem", backgroundColor:'transparent' }}
+                    elevation={0}
+                  >
+                    <div style={{display:'flex', gap:'2rem', flexDirection:'column', alignItems:'left'}}>
+                      <h4 style={{ fontSize: "35px",  }}>
+                      ¡Gestión Integral para tus actividades del semillero!
+                      </h4>
+                      <p>
+                        Añadir y gestionar actividades nunca fue tan fácil.
+                        Responde rápidamente y organiza tareas con nuestra
+                        interfaz amigable. ¡Simplifica tu vida semillero con
+                        cada clic!
+                      </p>
 
-                  <Paper style={{ width: '100%', borderRadius: '1.5rem' }} elevation={0} >
-
-
-                    <div>
-
-                      <h4 style={{ fontSize: '25px' }}>¡Optimiza tu Semillero con Nuestro Dashboard Intuitivo!</h4>
-                      <p>Añadir y gestionar actividades nunca fue tan fácil. Responde rápidamente y organiza tareas con nuestra interfaz amigable. ¡Simplifica tu vida semillero con cada clic!</p>
-
+                    <Button variant="outlined" style={{width:'fit-content'}}>Más Información</Button>
                     </div>
-
-
                   </Paper>
-
-
                 </Grid>
               </Grid>
-
-
-
-
-
-
-
-
-
             </Paper>
-
-
           </Grid>
 
-          <Grid item xs={12} sm={6} >
-
-            <Paper className={style.publicidadImg} sx={{
-              borderRadius: '1rem'
-            }} elevation={2}>
-
-
-              <Grid container spacing={5}>
-
-
+          <Grid  xs={12} sm={12} md={6} xl={3}>
+            <Paper
+              style={{ backgroundColor: "#B8CDF8" }}
+              className={style.publicidadImg}
+              sx={{}}
+              elevation={2}
+            >
+              <Grid
+                style={{ width: "90%", margin: "0 auto" }}
+                container
+                spacing={5}
+              >
                 <Grid xs={10} sm={6} item>
+                  <Paper
+                    
+                    style={{
+                      width:'fit-content',
+                      borderRadius: "1.5rem",
+                      backgroundColor:'transparent',
+                      padding:'1rem',
+                      display:'flex',
+                      flexDirection:'column',
+                      justifyContent:'center',
+                      alignItems:'center',
+                      backgroundColor:'black'
+                    }}
+                    elevation={1}
+                  >
 
-                  <Paper className={style.capturaFirst} style={{ width: '100%', height: '15rem', borderRadius: '1.5rem' }} elevation={1}>
 
+                    <LibraryBooksIcon style={{fontSize:'4rem', color:'white'}} />
                   </Paper>
-
-
                 </Grid>
 
                 <Grid xs={12} md={12} sm={6} item>
+                  <Paper
+                    style={{ width: "100%", borderRadius: "1.5rem", backgroundColor:'transparent' }}
+                    elevation={0}
+                  >
+                    <div style={{display:'flex', gap:'2rem', flexDirection:'column', alignItems:'left'}}>
+                      <h4 style={{ fontSize: "35px",  }}>
+                      Currículum Impecable: <br /> Registra Logros en el Semillero
+                      </h4>
+                      <p>
+                      Añade y organiza tus actividades del semillero con eficiencia, y observa cómo tu currículum cobra vida. Desde ponencias destacadas hasta proyectos innovadores
+                      </p>
 
-                  <Paper style={{ width: '100%', borderRadius: '1.5rem' }} elevation={0} >
-
-
-                    <div>
-
-                      <h4 style={{ fontSize: '25px' }}>¡Optimiza tu Semillero con Nuestro Dashboard Intuitivo!</h4>
-                      <p>Añadir y gestionar actividades nunca fue tan fácil. Responde rápidamente y organiza tareas con nuestra interfaz amigable. ¡Simplifica tu vida semillero con cada clic!</p>
-
+                    <Button variant="outlined" style={{width:'fit-content'}}>Más Información</Button>
                     </div>
-
-
                   </Paper>
-
-
                 </Grid>
-
-
               </Grid>
-
-
-
-
-
-
-
-
-
             </Paper>
-
-
-
-
           </Grid>
 
-          <Grid item xs={12} sm={6} >
-
-<Paper className={style.publicidadImg} sx={{
-  borderRadius: '1rem'
-}} elevation={2}>
-
-
-  <Grid container spacing={5}>
-
-    
-    <Grid xs={10} sm={6} item>
-
-      <Paper className={style.capturaFirst} style={{ width: '100%', height: '15rem', borderRadius: '1.5rem' }} elevation={1}>
-
-      </Paper>
-
-
-    </Grid>
-
-    <Grid xs={12} md={12} sm={6} item>
-
-      <Paper style={{ width: '100%', borderRadius: '1.5rem' }} elevation={0} >
-
-
-        <div>
-
-          <h4 style={{ fontSize: '25px' }}>¡Optimiza tu Semillero con Nuestro Dashboard Intuitivo!</h4>
-          <p>Añadir y gestionar actividades nunca fue tan fácil. Responde rápidamente y organiza tareas con nuestra interfaz amigable. ¡Simplifica tu vida semillero con cada clic!</p>
-
-        </div>
-
-
-      </Paper>
-
-
-    </Grid>
-
-
-  </Grid>
-
-
-
-
-
-
-
-
-
-</Paper>
-
-
-
-
-</Grid>
-
-<Grid item xs={12} sm={6} >
-
-<Paper className={style.publicidadImg} sx={{
-  borderRadius: '1rem'
-}} elevation={2}>
-
-
-  <Grid container spacing={5}>
-
-    
-    <Grid xs={10} sm={6} item>
-
-      <Paper className={style.capturaFirst} style={{ width: '100%', height: '15rem', borderRadius: '1.5rem' }} elevation={1}>
-
-      </Paper>
-
-
-    </Grid>
-
-    <Grid xs={12} md={12} sm={6} item>
-
-      <Paper style={{ width: '100%', borderRadius: '1.5rem' }} elevation={0} >
-
-
-        <div>
-
-          <h4 style={{ fontSize: '25px' }}>¡Optimiza tu Semillero con Nuestro Dashboard Intuitivo!</h4>
-          <p>Añadir y gestionar actividades nunca fue tan fácil. Responde rápidamente y organiza tareas con nuestra interfaz amigable. ¡Simplifica tu vida semillero con cada clic!</p>
-
-        </div>
-
-
-      </Paper>
-
-
-    </Grid>
-
-
-  </Grid>
-
-
-
-
-
-
-
-
-
-</Paper>
-
-
-
-
-</Grid>
-
-
+          <Grid  xs={12} sm={12} md={6} xl={3}>
+            <Paper
+              style={{ backgroundColor: "#95F2D9" }}
+              className={style.publicidadImg}
+              sx={{}}
+              elevation={2}
+            >
+              <Grid
+                style={{ width: "90%", margin: "0 auto" }}
+                container
+                spacing={5}
+              >
+                <Grid xs={10} sm={6} item>
+                  <Paper
+                    
+                    style={{
+                      width:'fit-content',
+                      borderRadius: "1.5rem",
+                      backgroundColor:'transparent',
+                      padding:'1rem',
+                      display:'flex',
+                      flexDirection:'column',
+                      justifyContent:'center',
+                      alignItems:'center'
+                    }}
+                    elevation={1}
+                  >
+
+
+                    <DvrIcon style={{fontSize:'4rem'}} />
+                  </Paper>
+                </Grid>
+
+                <Grid xs={12} md={12} sm={6} item>
+                  <Paper
+                    style={{ width: "100%", borderRadius: "1.5rem", backgroundColor:'transparent' }}
+                    elevation={0}
+                  >
+                    <div style={{display:'flex', gap:'2rem', flexDirection:'column', alignItems:'left'}}>
+                      <h4 style={{ fontSize: "35px",  }}>
+                      ¡Gestión Integral para tus actividades del semillero!
+                      </h4>
+                      <p>
+                        Añadir y gestionar actividades nunca fue tan fácil.
+                        Responde rápidamente y organiza tareas con nuestra
+                        interfaz amigable. ¡Simplifica tu vida semillero con
+                        cada clic!
+                      </p>
+
+                    <Button variant="outlined" style={{width:'fit-content'}}>Más Información</Button>
+                    </div>
+                  </Paper>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+
+          <Grid  xs={12} sm={12} md={6} xl={3}>
+            <Paper
+              style={{ backgroundColor: "#9D8DF1" }}
+              className={style.publicidadImg}
+              sx={{}}
+              elevation={2}
+            >
+              <Grid
+                style={{ width: "90%", margin: "0 auto" }}
+                container
+                spacing={5}
+              >
+                <Grid xs={10} sm={6} item>
+                  <Paper
+                    
+                    style={{
+                      width:'fit-content',
+                      borderRadius: "1.5rem",
+                      backgroundColor:'transparent',
+                      padding:'1rem',
+                      display:'flex',
+                      flexDirection:'column',
+                      justifyContent:'center',
+                      alignItems:'center'
+                    }}
+                    elevation={1}
+                  >
+
+
+                    <DvrIcon style={{fontSize:'4rem'}} />
+                  </Paper>
+                </Grid>
+
+                <Grid xs={12} md={12} sm={6} item>
+                  <Paper
+                    style={{ width: "100%", borderRadius: "1.5rem", backgroundColor:'transparent' }}
+                    elevation={0}
+                  >
+                    <div style={{display:'flex', gap:'2rem', flexDirection:'column', alignItems:'left'}}>
+                      <h4 style={{ fontSize: "35px",  }}>
+                      ¡Gestión Integral para tus actividades del semillero!
+                      </h4>
+                      <p>
+                        Añadir y gestionar actividades nunca fue tan fácil.
+                        Responde rápidamente y organiza tareas con nuestra
+                        interfaz amigable. ¡Simplifica tu vida semillero con
+                        cada clic!
+                      </p>
+
+                    <Button variant="outlined" style={{width:'fit-content'}}>Más Información</Button>
+                    </div>
+                  </Paper>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
         </Grid>
-
-
-
-
       </Box>
-
     </>
-
   );
 }
 
